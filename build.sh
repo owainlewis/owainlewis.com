daa478e11eb8a10103896666beba8a45a161ead7
Owain Lewis <owain@owainlewis.com>
2017-07-23 08:45:04 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for path in _posts/*.md; do
-file=${path##*/}
-filename="${file%.*}"
-pandoc \
--H static/html/header.html \
--A static/html/footer.html \
-${path} \
--o posts/${filename}.html
+  FILE=${path##*/}
+  NAME="${FILE%.*}"
+  pandoc \
+  -H static/html/header.html \
+  -A static/html/footer.html \
+  ${path} \
+  -o posts/${NAME}.html
 done
